ed37e8b246196e672f5405b040d208707bed08ea
romi.indan@gmail.com <romi.indan@gmail.com>
2024-07-24 00:35:17 +0530

    row/column and modifyer

--- app/src/main/java/com/example/mycomposeapplications/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapplications/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.mycomposeapplications
 
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mycomposeapplications.ui.theme.MyComposeApplicationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,13 +34,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TextAreaView()
                 }
             }
         }
     }
 }
 
+@Composable
+fun TextAreaView(){
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp).background(Color.Yellow,
+            RoundedCornerShape(6.dp)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Name:XYZ", fontSize = 16.sp, modifier = Modifier.padding(16.dp))
+        Text(text = "Age:30",fontSize = 16.sp, modifier = Modifier.padding(16.dp))
+        Row (){
+            Text(text = "Gender:Male",fontSize = 16.sp, modifier = Modifier.padding(16.dp))
+            Text(text = "Gender:Female",fontSize = 16.sp, modifier = Modifier.padding(16.dp))
+        }
+
+    }
+
+
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
