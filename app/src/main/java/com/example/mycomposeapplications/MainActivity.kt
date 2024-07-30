8fa7acb7021cedc7eec1b3c1acb599d7887b684f
romi.indan@gmail.com <romi.indan@gmail.com>
2024-07-31 00:24:17 +0530

    list view with id and name

--- app/src/main/java/com/example/mycomposeapplications/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapplications/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,26 +20,33 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -55,6 +63,8 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -71,17 +81,7 @@ class MainActivity : ComponentActivity() {
 //                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(modifier = Modifier
-                        .padding(16.dp)
-                        .verticalScroll(rememberScrollState(),true)
-                        ) {
-                        ImagesStyles()
-                        RoundedCornerImages()
-                        CircleImage()
-                        CircleImage()
-                        CircleImage()
-                        CircleImage()
-                    }
+                    ListViewsData()
 
 
                 }
@@ -89,6 +89,82 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Preview
+@Composable
+fun CustomeList(){
+
+}
+@Preview
+@Composable
+ fun ListViewsData() {
+     var context= LocalContext.current
+    var personList = listOf(
+        "Ruth",
+        "Dilip",
+        "Suresh",
+        "Ramesh",
+        "Dinesh",
+        "Ganesh",
+        "Radhika",
+        "Raunak"
+    )
+    LazyColumn {
+        //only print name of click event
+//          items(
+//              items=personList,
+//              itemContent = {person ->
+//                  androidx.compose.material3.Text(
+//                  text = person,
+//                  modifier = Modifier.padding(16.dp)
+//                      .clickable {
+//                          Toast.makeText(context,"$person",Toast.LENGTH_SHORT).show()
+//                      }
+//              )
+//                  Divider()//---> vertical and horizontal line
+//              }
+//
+//          )
+        // name and index item position click
+        itemsIndexed(personList){
+            index, item ->
+            androidx.compose.material3.Text(
+                text = "$index. $item",
+                modifier = Modifier.padding(16.dp)
+                    .clickable {
+                        Toast.makeText(context,"index:-${index}-$item",Toast.LENGTH_SHORT).show()
+                    }
+            )
+            Divider()//---> vertical and horizontal line
+
+        }
+
+    }
+}
+
+
+@Preview
+@Composable
+ fun EditTextOrTextField() {
+    var text by remember {
+        mutableStateOf("")
+    }
+    TextField(value = text, onValueChange = {
+        text=it
+    },
+        textStyle = TextStyle(color=Color.Red),
+        keyboardOptions = KeyboardOptions(
+            capitalization = KeyboardCapitalization.Characters,
+            autoCorrect = true,
+            keyboardType = KeyboardType.Text
+        ),
+        placeholder = {
+            Text(text = "Enter the name")
+        },
+        singleLine = true
+    )
+
+}
+
 @Preview
 @Composable
 fun ImagesStyles(){
